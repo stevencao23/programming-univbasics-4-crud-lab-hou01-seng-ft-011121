5de4a9244a29ed1d7ae9e0c21dc21150ffa04caf
steven cao <stevencao23@gmail.com>
2020-12-09 05:00:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  add_element_to_start_of_array = ["I", "am", ]
+  add_element_to_start_of_array = ["I", "am", "really", "learning"]
+  add_element_to_start_of_array.unshift("wow")
   
 end
 
