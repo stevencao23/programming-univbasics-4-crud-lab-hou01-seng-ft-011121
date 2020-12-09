02f5bc2d1278c55dace177c81ae087dcfb84caae
steven cao <stevencao23@gmail.com>
2020-12-09 04:47:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  create_an_array = []
+  create_an_array = ["example1", "example2", "example3", "example4"]
 end
 
 def add_element_to_end_of_array(array, element)
