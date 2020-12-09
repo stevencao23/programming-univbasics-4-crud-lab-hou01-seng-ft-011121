48604b32ccf182fb21e897cc92bb7317761d67fd
steven cao <stevencao23@gmail.com>
2020-12-09 05:07:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,7 @@ end
 
 def remove_element_from_end_of_array(array)
   remove_element_from_end_of_array = ["I", "am", "really", "learning", "arrays!"]
-  array = remove_element_from_start_of_array.pop
+  remove_element_from_end_of_array.pop
   
 end
 
